7c985c3a1f37e50300b54e4cfd4fab9ad9e1af32
MBrill <manfred.brill@hs-kl.de>
2020-11-12 09:40:16 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Logging in Unity
+
+Verwenden von Logging-Frameworks wie log4net in Unity-Projekten.
+
+
+![Lizenzlogo](https://licensebuttons.net/l/by-nc-sa/3.0/de/88x31.png)
---
+++ log4net/Assets/Scripts/LoggingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using log4net.Config;
+using UnityEngine;
+
+public static class LoggingConfiguration
+{
+    // Mit RuntimeInitializeLoadType.BeforeSceneLoad stellen wir sicher,
+    // dass wir Log4Net konfigurieren, bevor die Szene geladen wird.
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void ConfigureLogging()
+    {
+        XmlConfigurator.Configure(new FileInfo($"{Application.dataPath}/log4net.xml"));
+    }
+}
